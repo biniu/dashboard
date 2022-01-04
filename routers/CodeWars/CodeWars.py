bbaadd8466b2b852c55f6b1d143a145dcbe59e0e
biniu <dawid.binkowski93@gmail.com>
2022-01-04 17:17:43 +0100

    add check for creating language scores add new read for one language

--- routers/CodeWars/CodeWars.py
+++ routers/CodeWars/CodeWars.py
@@ -47,8 +47,9 @@ class LanguageScore(BaseModel):
     score: int
     rank: int
 
-    user_id: int
     lang_id: int
+
+    last_update: Optional[date]
 
 
 @router.get("/users")
@@ -67,8 +68,8 @@ async def create_user(user: CodeWarsUser, db: Session = Depends(get_db)):
     db.add(user_info_model)
     db.commit()
 
-    created_user = db.query(CodeWarsModels.CodeWarsUsers).\
-        filter(CodeWarsModels.CodeWarsUsers.name == user.name)\
+    created_user = db.query(CodeWarsModels.CodeWarsUsers). \
+        filter(CodeWarsModels.CodeWarsUsers.name == user.name) \
         .first()
 
     print(created_user)
@@ -93,8 +94,6 @@ async def read_user_statistics(user_id: int, db: Session = Depends(get_db)):
 
 @router.post("/UserStatistics/{user_id}")
 async def create_user_statistics(user_id: int, user_statistics: CodeWarsUserStatistic, db: Session = Depends(get_db)):
-    print("create_user_statistics")
-
     if not db.query(CodeWarsModels.CodeWarsUsers) \
             .filter(CodeWarsModels.CodeWarsUsers.id == user_id).first():
         raise HTTPException(status_code=400, detail=f"User with ID {user_id} not exist")
@@ -105,10 +104,6 @@ async def create_user_statistics(user_id: int, user_statistics: CodeWarsUserStat
     else:
         print("create new date")
         statistic_date = datetime.today().strftime('%Y-%m-%d')
-
-    print("*"*80)
-    print(repr(statistic_date))
-    print("*" * 80)
 
     last_update = db.query(CodeWarsModels.CodeWarsUserStatistics) \
         .filter(CodeWarsModels.CodeWarsUserStatistics.last_update == statistic_date)
@@ -149,8 +144,8 @@ async def create_language_infos(language_info: LanguageInfo, db: Session = Depen
     db.add(language_info_model)
     db.commit()
 
-    created_language = db.query(CodeWarsModels.LanguageInfos).\
-        filter(CodeWarsModels.LanguageInfos.name == language_info.name)\
+    created_language = db.query(CodeWarsModels.LanguageInfos). \
+        filter(CodeWarsModels.LanguageInfos.name == language_info.name) \
         .first()
 
     print(created_language)
@@ -162,19 +157,66 @@ async def create_language_infos(language_info: LanguageInfo, db: Session = Depen
     }
 
 
+@router.get("/LanguageScores/{user_id}")
+async def read_language_scores(user_id: int, db: Session = Depends(get_db)):
+    if not db.query(CodeWarsModels.CodeWarsUsers) \
+            .filter(CodeWarsModels.CodeWarsUsers.id == user_id).first():
+        raise HTTPException(status_code=400, detail=f"User with ID {user_id} not exist")
 
-@router.get("/LanguageScores")
-async def read_language_scores(db: Session = Depends(get_db)):
-    return db.query(CodeWarsModels.LanguageScores).all()
+    return db.query(CodeWarsModels.LanguageScores) \
+        .filter(CodeWarsModels.LanguageScores.user_id == user_id).all()
 
 
-@router.post("/LanguageScores")
-async def create_language_scores(language_score: LanguageScore, db: Session = Depends(get_db)):
-    language_score_model = CodeWarsModels.LanguageScores()
+@router.get("/LanguageScores/{user_id}/{lang_id}")
+async def read_language_scores(user_id: int, lang_id: int, db: Session = Depends(get_db)):
+    if not db.query(CodeWarsModels.CodeWarsUsers) \
+            .filter(CodeWarsModels.CodeWarsUsers.id == user_id).first():
+        raise HTTPException(status_code=400, detail=f"User with ID {user_id} not exist")
+
+    if not db.query(CodeWarsModels.LanguageInfos) \
+            .filter(CodeWarsModels.LanguageInfos.id == lang_id).first():
+        raise HTTPException(status_code=400, detail=f"Lang with ID {lang_id} not exist")
+
+    return db.query(CodeWarsModels.LanguageScores) \
+        .filter(CodeWarsModels.LanguageScores.lang_id == lang_id) \
+        .filter(CodeWarsModels.LanguageScores.user_id == user_id).all()
+
+
+@router.post("/LanguageScores/{user_id}")
+async def create_language_scores(user_id: int, language_score: LanguageScore, db: Session = Depends(get_db)):
+    if not db.query(CodeWarsModels.CodeWarsUsers) \
+            .filter(CodeWarsModels.CodeWarsUsers.id == user_id).first():
+        raise HTTPException(status_code=400, detail=f"User with ID {user_id} not exist")
+
+    if not db.query(CodeWarsModels.LanguageInfos) \
+            .filter(CodeWarsModels.LanguageInfos.id == language_score.lang_id).first():
+        raise HTTPException(status_code=400, detail=f"Lang with ID {language_score.lang_id} not exist")
+
+    if language_score.last_update:
+        print("data from parm")
+        statistic_date = language_score.last_update
+    else:
+        print("create new date")
+        statistic_date = datetime.today().strftime('%Y-%m-%d')
+
+    last_update = db.query(CodeWarsModels.LanguageScores) \
+        .filter(CodeWarsModels.LanguageScores.last_update == statistic_date) \
+        .filter(CodeWarsModels.LanguageScores.user_id == user_id)
+
+    if last_update.first():
+        print(f"Lang statistic for {statistic_date} {user_id} exist -> update exist one")
+        language_score_model = last_update.first()
+    else:
+        print(f"Lang statistic for {statistic_date} {user_id} not exist -> create new one")
+        language_score_model = CodeWarsModels.LanguageScores()
+
     language_score_model.score = language_score.score
     language_score_model.rank = language_score.rank
-    language_score_model.user_id = language_score.user_id
+    language_score_model.user_id = user_id
     language_score_model.lang_id = language_score.lang_id
+
+    if language_score.last_update:
+        language_score_model.last_update = statistic_date
 
     db.add(language_score_model)
     db.commit()
