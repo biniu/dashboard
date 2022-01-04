6aa1fae572011f63cda45270cfb9cfcb75d453d6
biniu <dawid.binkowski93@gmail.com>
2022-01-04 17:46:40 +0100

    add sync for lang statistics

--- routers/CodeWars/CodeWarsSync.py
+++ routers/CodeWars/CodeWarsSync.py
@@ -6,7 +6,7 @@ import json
 from CodeWarsInterface import CodeWarsInterface
 
 
-def date_get(url: str):
+def data_get(url: str):
     with requests.Session() as session:
         session = requests.Session()
         response = session.get(url)
@@ -21,7 +21,7 @@ def date_get(url: str):
         raise requests.HTTPError
 
 
-def date_post(url: str, data: json):
+def data_post(url: str, data: json):
     with requests.Session() as session:
         session = requests.Session()
         response = session.post(url, json=data)
@@ -41,7 +41,7 @@ def sync() -> None:
 
     code_wars_client = CodeWarsInterface(user_name)
 
-    users = date_get(url="http://127.0.0.1:8000/CodeWars/users")
+    users = data_get(url="http://127.0.0.1:8000/CodeWars/users")
     user_exist = list(filter(lambda user: user['name'] == user_name, users))
 
     if user_exist:
@@ -49,8 +49,11 @@ def sync() -> None:
         user_id = user_exist[0]['id']
     else:
         print("Creating user")
-        data = date_post(url="http://127.0.0.1:8000/CodeWars/users", data={"name": "biniu"})
+        data = data_post(url="http://127.0.0.1:8000/CodeWars/users", data={"name": user_name})
         user_id = data['user_id']
+
+
+    print(f"user_id {user_id}")
 
     data = {
       "honor": code_wars_client.get_user_honor(),
@@ -59,12 +62,43 @@ def sync() -> None:
       "last_update": datetime.today().strftime('%Y-%m-%d')
     }
 
-    date_post(
+    data_post(
         url=f"http://127.0.0.1:8000/CodeWars/UserStatistics/{user_id}",
         data=data
     )
 
-    pprint(date_get(f"http://127.0.0.1:8000/CodeWars/UserStatistics/{user_id}"))
+    pprint(data_get(f"http://127.0.0.1:8000/CodeWars/UserStatistics/{user_id}"))
+
+    languages = code_wars_client.get_language_list()
+    create_languages = data_get(url="http://127.0.0.1:8000/CodeWars/LanguageInfos")
+    print(create_languages)
+
+    for language in languages:
+        print(language)
+        lang_exist = list(filter(lambda lang: lang['name'] == language, create_languages))
+        if lang_exist:
+            lang_id = lang_exist[0]['id']
+        else:
+            data = data_post(
+                url="http://127.0.0.1:8000/CodeWars/LanguageInfos",
+                data={"name": language}
+            )
+            lang_id = data['id']
+
+        print(f"lang_id {lang_id}")
+        lang_stats = code_wars_client.get_language_statistics(language)
+        out = data_post(
+            url=f"http://127.0.0.1:8000/CodeWars/LanguageScores/{user_id}",
+            data={
+                "score": lang_stats['score'],
+                "rank": lang_stats['rank'],
+                "lang_id": lang_id
+            }
+        )
+
+        print(out)
+
+    # pprint(data_get(url="http://127.0.0.1:8000/CodeWars/LanguageInfos"))
 
 
 if __name__ == '__main__':
