c1c12562d354f2093b5978c53a0966d1d74f1691
biniu <dawid.binkowski93@gmail.com>
2022-06-09 19:58:40 +0200

    move get_db func add DEBUG option for db engine chose

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,20 +7,31 @@ from sqlalchemy.ext.declarative import declarative_base
 # to connect to DB
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
-# # create engine
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
+print("MODE -----------------------")
+print(f"DEBUG: {os.getenv('DEBUG')}")
 
-user = os.getenv("POSTGRES_USER")
-password = os.getenv("POSTGRES_PASSWORD")
-db = os.getenv("POSTGRES_DB")
-
-engine = create_engine(
-    f"postgresql://{user}:{password}@postgres_db_container:5432/{db}"
+if os.getenv('DEBUG') == "true":
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
+else:
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    db = os.getenv("POSTGRES_DB")
+
+    engine = create_engine(
+        f"postgresql://{user}:{password}@postgres_db_container:5432/{db}"
+        )
 
 # create session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
